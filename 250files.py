eeb35db73f1cc2055881c468b608a606e22f194f
raktimgg <44519382+raktimgg@users.noreply.github.com>
2018-11-19 15:13:05 +0530

    Create 250files.py

---
+++ 250files.py
@@ -0,0 +1,20 @@
+import numpy as np
+import soundfile as sf
+new_data = np.empty([25000,]) #creating an empty array for new file to be generated from original file
+y1 = np.empty([25000,])
+for j in range(1,3):
+	b= "hello"+str(j)+".wav"
+	data, samplerate = sf.read(b) #reading audio file using soundfile library
+	print len(data), samplerate
+	x= len(data)
+	p = 25000-x
+	for y in range(1 ,p):   
+		for i in range(0,y-1):      #adding empty elements in the array in the start
+			new_data[i] =y1[i]
+		for i in range(y,25000-x+y-1):
+			new_data[i] =data[i-y]
+		for i in range(25000-y , 24999):    #adding empty elements in the array in the end 
+			new_data[i] = y1[i]	
+		a = "hello__"+str(j) +"_"+str(y)+".wav"    #total length becomes 25000
+		sf.write(a, new_data, samplerate)  #audio files are written back to harddisk
+		print len(new_data)
